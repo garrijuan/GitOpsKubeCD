c95d50029adc1ff4f2a7c51251b57c804057d655
garrijuan <garrijuan@gmail.com>
2023-11-13 20:46:22 +0100

    init commit

---
+++ app-python/README.md
@@ -0,0 +1,11 @@
+## correr app en local
+```sh
+uvicorn main:app --reload
+```
+
+## correr test 
+```sh
+python3 test_main.py
+
+python3 -m unittest test_main.py
+```
---
+++ app-python/app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+# Crear una instancia de la aplicación FastAPI
+app = FastAPI()
+
+# Definir una ruta para la raíz de la aplicación
+@app.get("/")
+def read_root():
+    return {"Hola": "Mundo"}
